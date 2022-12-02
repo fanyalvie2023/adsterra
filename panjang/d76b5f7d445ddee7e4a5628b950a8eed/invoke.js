c4e640ec83415d64ebfdffed7cb4030a8e58fbeb
fanyalvie2023 <115063283+fanyalvie2023@users.noreply.github.com>
2022-12-02 18:38:52 +0700

    Update invoke.js

--- panjang/d76b5f7d445ddee7e4a5628b950a8eed/invoke.js
+++ panjang/d76b5f7d445ddee7e4a5628b950a8eed/invoke.js
@@ -6,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/d76b5f7d445ddee7e4a5628b950a8eed/invoke.js"></scr' + 'ipt>');
-</script>
